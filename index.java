4719dd6df4626394e4f4a277f1f533d22f8a92c1
HARIPRASAD S A <myharicode@gmail.com>
2021-07-07 17:42:22 +0530

    commited

--- index.java
+++ index.java
@@ -2,5 +2,6 @@ import java.util.*;
 class index{
     public static void main(String[] args) {
         System.out.println("Java hello");
+        System.out.println("Hello this is hari");
     }
 }
